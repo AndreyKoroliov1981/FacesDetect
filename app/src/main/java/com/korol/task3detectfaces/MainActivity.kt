cb13db119c47cb6129c6fe81cef5fc29afd2af87
Андрей Королёв <korol81@bk.ru>
2022-08-06 16:05:00 +0300

    first commit

---
+++ app/src/main/java/com/korol/task3detectfaces/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.korol.task3detectfaces
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.korol.task3detectfaces.camerax.CameraManager
+import com.korol.task3detectfaces.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var cameraManager: CameraManager
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        createCameraManager()
+        checkForPermission()
+        onClicks()
+    }
+
+    private fun checkForPermission() {
+        if (allPermissionsGranted()) {
+            cameraManager.startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun onClicks() {
+        binding.btnSwitch.setOnClickListener {
+            cameraManager.changeCameraSelector()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                cameraManager.startCamera()
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT)
+                    .show()
+                finish()
+            }
+        }
+    }
+
+    private fun createCameraManager() {
+        cameraManager = CameraManager(
+            this,
+            binding.previewViewFinder,
+            this,
+            binding.graphicOverlayFinder
+        )
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+    }
+
+}
